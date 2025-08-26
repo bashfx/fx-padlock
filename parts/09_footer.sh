481ad371af96a2b1508fa4362f7b7d3c2249cdd6
qodeninja <1043235+qodeninja@users.noreply.github.com@users.noreply.github.com>
2025-08-26 05:48:40 +0000

    Remove plaintext files - encrypted in .chest/

--- parts/09_footer.sh
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Footer - Script Invocation
-################################################################################
-
-# Only run main if script is executed directly (not sourced)
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main "$@"
-fi
